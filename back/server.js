732a79aff51607e378b2cf50fddddbd437d21b48
Pavel Troshin <troshpaw@gmail.com>
2024-08-06 13:01:32 +0300

    create sever in progress 50%

--- back/server.js
+++ back/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import {start, getGameStatus, getGooglePosition, playAgain} from '../core/state-manager.js';
+import {start, playAgain, movePlayer, getGooglePoints, getPlayerPoints, getGameStatus} from '../core/state-manager.js';
+import {getGridSize, getGooglePosition, getPlayerPosition} from '../core/state-manager.js';
 
 const app = express();
 const port = 3000;
@@ -14,15 +15,41 @@ app.get('/playAgain', async (req, res) => {
     res.send(200);
 });
 
+app.get('/movePlayer', async (req, res) => {
+    await movePlayer(playerNumber, direction);
+    res.send(200);
+});
+
+app.get('/getGooglePoints', async (req, res) => {
+    const googlePoints = await getGooglePoints();
+    res.send({data: googlePoints});
+});
+
+app.get('/getPlayerPoints', async (req, res) => {
+    const playerPoints = await getPlayerPoints();
+    res.send({data: playerPoints});
+});
+
 app.get('/getGameStatus', async (req, res) => {
     const gameStatus = await getGameStatus();
-    res.send(gameStatus);
+    res.send({data: gameStatus});
+});
+
+app.get('/getGridSize', async (req, res) => {
+    const gridSize = await getGridSize();
+    res.send({data: gridSize});
 });
 
 app.get('/getGooglePosition', async (req, res) => {
     const googlePosition = await getGooglePosition();
-    res.send(googlePosition );
+    res.send({data: googlePosition});
 });
+
+app.get('/getPlayerPosition', async (req, res) => {
+    const playerPosition = await getPlayerPosition();
+    res.send({data: playerPosition});
+});
+
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
