d5d9f81c11b474f778484accdb2c853c6d4e7299
Pavel <troshpaw@gmail.com>
2024-08-06 11:48:37 +0300

    create server in progress

--- back/server.js
+++ back/server.js
@@ -1,10 +1,27 @@
 import express from 'express';
+import {start, getGameStatus, getGooglePosition, playAgain} from '../core/state-manager.js';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+app.get('/start', async (req, res) => {
+    await start();
+    res.send(200);
+});
+
+app.get('/playAgain', async (req, res) => {
+    await playAgain();
+    res.send(200);
+});
+
+app.get('/getGameStatus', async (req, res) => {
+    const gameStatus = await getGameStatus();
+    res.send(gameStatus);
+});
+
+app.get('/getGooglePosition', async (req, res) => {
+    const googlePosition = await getGooglePosition();
+    res.send(googlePosition );
 });
 
 app.listen(port, () => {
